0f24b8c12b41a7bd892d62a296862d7bbb5de484
ReformedCola <reformedcola@gmail.com>
2020-05-06 16:51:18 -0700

    global component <Nav/>

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,28 @@
 <template>
   <div>
     <router-view/>
-    <hr/>
-    <div>
-      <router-link to="/labels">Tags</router-link>
-      |
-      <router-link to="/money">Expense</router-link>
-      |
-      <router-link to="/statistics">Statistics</router-link>
-    </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-link to="/labels">Tags</router-link>
+    |
+    <router-link to="/money">Expense</router-link>
+    |
+    <router-link to="/statistics">Statistics</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Nav extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
