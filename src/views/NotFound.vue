2ad0ccbc2240e640fefadd9121ca27b342765086
ReformedCola <reformedcola@gmail.com>
2020-05-06 17:08:11 -0700

    added 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>SORRY, PAGE NOT FOUND</div>
+    <router-link to="/">Back to Homepage</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NotFound extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
