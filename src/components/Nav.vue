25c9026718a4f99457c0f5d6cb8a9dfb707533fd
ReformedCola <reformedcola@gmail.com>
2020-05-08 21:28:10 -0700

    added scss for <Nav/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/labels">
+  <nav>
+    <router-link to="/labels" class="item">
       <Icon name="label"/>
       Tags
     </router-link>
-    |
-    <router-link to="/money">
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       Expense
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       Statistics
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -28,7 +26,22 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid red;
+  nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
   }
 </style>
