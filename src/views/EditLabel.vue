65dec4f3326a41319e9f71a2a3bce8ef71c13c5e
ReformedCola <reformedcola@gmail.com>
2020-05-14 15:24:02 -0700

    change tag's string into {id, name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    Edit Label
+  </Layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class EditLabel extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
