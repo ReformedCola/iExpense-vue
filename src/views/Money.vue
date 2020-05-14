8ede14ddb3c1763e42228530301f99bf9a754b4d
ReformedCola <reformedcola@gmail.com>
2020-05-14 10:43:33 -0700

    encapsulated model.ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,7 +6,6 @@
     <Notes :value.sync="record.notes"/>
     <Tags :data-source.sync="tags"
           :value.sync="record.tags"/>
-    {{recordList}}
   </Layout>
 </template>
 
@@ -17,36 +16,29 @@
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
+  import {model} from '@/model';
 
-  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-
-  type Record = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
-    createdAt?: Date;
-  }
+  const recordList = model.fetch();
 
   @Component({
     components: {Tags, Notes, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = ['Clothing', 'Food', 'Housing', 'Travel'];
-    recordList: Record[] = recordList;
-    record: Record = {
+    recordList: RecordItem[] = recordList;
+    record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
 
     saveRecord() {
-      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      const record2 = model.clone(this.record);
       record2.createdAt = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+      model.save(this.recordList);
     }
   }
 </script>
