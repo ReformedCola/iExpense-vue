61197dac6b19ec02967aa40d50e3330eab1017db
ReformedCola <reformedcola@gmail.com>
2020-05-15 16:11:34 -0700

    display name of tag

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="Label" placeholder="Type your label name here ~"/>
+      <FormItem :value="tag.name" field-name="Label" placeholder="Type your label name here ~"/>
     </div>
     <div class="button-wrapper">
       <Button>Delete Label</Button>
@@ -25,13 +25,15 @@
     components: {Button, FormItem}
   })
   export default class EditLabel extends Vue {
+    tag?: { id: string; name: string } = undefined;
+
     created() {
       const id = this.$route.params.id;
       tagListModel.fetch();
       const tags = tagListModel.data;
       const tag = tags.filter(t => t.id === id)[0];
       if (tag) {
-        console.log(tag);
+        this.tag = tag;
       } else {
         this.$router.replace('/404');
       }
@@ -62,10 +64,12 @@
       height: 24px;
     }
   }
+
   .form-wrapper {
     background: white;
     margin-top: 8px;
   }
+
   .button-wrapper {
     text-align: center;
     padding: 16px;
