7f89cbc2b09e9f00eae20aedd4d77532ab7d1d13
ReformedCola <reformedcola@gmail.com>
2020-05-15 20:50:06 -0700

    encapsulated CRUD of tagList

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,9 @@ Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
 window.tagList = tagListModel.fetch();
+window.findTag = (id: string) => {
+  return window.tagList.filter(t => t.id === id)[0];
+};
 window.createTag = (name: string) => {
   const message = tagListModel.create(name);
   if (message === 'duplicated') {
@@ -22,7 +25,13 @@ window.createTag = (name: string) => {
   } else if (message === 'success') {
     window.alert('Added Successfully');
   }
-}
+};
+window.removeTag = (id: string) => {
+  return tagListModel.remove(id);
+};
+window.updateTag = (id: string, name: string) => {
+  return tagListModel.update(id, name);
+};
 
 new Vue({
   router,
