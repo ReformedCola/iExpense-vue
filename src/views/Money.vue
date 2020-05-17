681989ca7ec3e4097a5329e0432f3f0ddce26036
ReformedCola <reformedcola@gmail.com>
2020-05-16 17:27:41 -0700

    change all computed to getter

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,13 +22,12 @@
 
   @Component({
     components: {Tags, FormItem, Types, NumberPad},
-    computed: {
-      recordList() {
-        return this.$store.state.recordList;
-      }
-    }
   })
   export default class Money extends Vue {
+    get recordList() {
+      return this.$store.state.recordList;
+    }
+
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
