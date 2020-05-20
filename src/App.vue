92cfa613e8cac46b33c4fda012a8b82c920b6671
ReformedCola <reformedcola@gmail.com>
2020-05-19 18:06:01 -0700

    Fixed typo

--- src/App.vue
+++ src/App.vue
@@ -5,46 +5,49 @@
 </template>
 
 <script lang="js">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
 
-  @Component
-  export default class App extends Vue {
-    beforeCreate () {
-      window.onload = function () {
-        document.addEventListener('touchstart', function (event) {
-          if (event.touches.length > 1) {
-            event.preventDefault();
-          }
-        });
-        document.addEventListener('gesturestart', function (event) {
-          event.preventDefault();
-        });}
+@Component
+export default class App extends Vue {
+  beforeCreate() {
+    window.onload = function () {
+      document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+          event.preventDefault()
+        }
+      })
+      document.addEventListener('gesturestart', function (event) {
+        event.preventDefault()
+      })
     }
-
   }
+
+}
 
 </script>
 
 <style lang="scss">
-  @import '~@/assets/style/helper.scss';
-  @import '~@/assets/style/reset.scss';
+@import '~@/assets/style/helper.scss';
+@import '~@/assets/style/reset.scss';
 
-  body {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #333333;
-    line-height: 1.5;
-    font-family: $font-hei;
-    font-size: 16px;
-    /*background: #f5f5f5;*/
-    background: #ffffff;
-    text-align: center;
-    &::-webkit-scrollbar {
-      width: 0
-    }
-    -ms-overflow-style: none;
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333333;
+  line-height: 1.5;
+  font-family: $font-hei;
+  font-size: 16px;
+  /*background: #f5f5f5;*/
+  background: #ffffff;
+  text-align: center;
+
+  &::-webkit-scrollbar {
+    width: 0
   }
+
+  -ms-overflow-style: none;
+}
 
 
 </style>
